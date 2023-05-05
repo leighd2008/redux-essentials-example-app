12b89cd4c3d1871c33feb24687a1978f1b144bba
Diane Leigh <leighd2008@gmail.com>
2023-05-05 11:44:26 -0400

    showing new notifications

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import { fetchNotifications } from '../features/notifications/notificationsSlice'
+import { fetchNotifications, selectAllNotifications } from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
   const dispatch = useDispatch()
+  const notifications = useSelector(selectAllNotifications)
+  const numUnreadNotifications = notifications.filter(n => !n.isNew).length
   
   const fetchNewNotifications = () => {
     dispatch(fetchNotifications())
+  }
+  
+  let unreadNotificationsBadge = 0
+  
+  if (numUnreadNotifications > 0) {
+    unreadNotificationsBadge = (
+      <span className='badge' >{numUnreadNotifications}</span>
+    )
   }
   
   return (
@@ -20,7 +30,9 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/" >Posts</Link>
             <Link to="/users" >Users</Link>
-            <Link to="/notifications">Notifications</Link>
+            <Link to="/notifications">
+              Notifications {unreadNotificationsBadge}
+            </Link>
           </div>
           <button className='button' onClick={fetchNewNotifications}>
             Refresh Notifications
